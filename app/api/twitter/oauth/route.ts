da6dec0328bf24ef92692c05a1ab13b997b154fe
Manish Choudhary <manishchoudhary9663@gmail.com>
2024-11-13 14:37:04 +0530

    Update route.ts

--- app/api/twitter/oauth/route.ts
+++ app/api/twitter/oauth/route.ts
@@ -1,25 +1,24 @@
-// pages/api/twitter/oauth.ts
+// app/api/twitter/oauth/route.ts
 import { TwitterApi } from "twitter-api-v2";
-import { setCookie } from "cookies-next";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+const CALLBACK_URL = `${process.env.NEXT_PUBLIC_APP_URL}/api/twitter/callback`;
+
+export async function GET() {
   const twitterClient = new TwitterApi({
     clientId: process.env.TWITTER_CLIENT_ID!,
     clientSecret: process.env.TWITTER_CLIENT_SECRET!,
   });
 
   const { url, codeVerifier, state } = twitterClient.generateOAuth2AuthLink(
-    `${process.env.NEXT_PUBLIC_APP_URL}/api/twitter/callback`,
+    CALLBACK_URL,
     { scope: ["tweet.read", "users.read", "follows.read"] }
   );
 
   // Store codeVerifier and state in cookies for verification upon callback
-  setCookie("codeVerifier", codeVerifier, { req, res });
-  setCookie("state", state, { req, res });
+  cookies().set("codeVerifier", codeVerifier);
+  cookies().set("state", state);
 
-  res.redirect(url);
+  return NextResponse.redirect(url);
 }
